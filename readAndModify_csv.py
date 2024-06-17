093742ded834eaf2ec505feb99dcd3c51fb1a3d6
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-06-17 10:12:35 +0200

    chore: Update input and output file names for CSV processing

--- readAndModify_csv.py
+++ readAndModify_csv.py
@@ -4,8 +4,8 @@ import random
 from datetime import datetime, timedelta
 import os
 
-input_file = 'cresta_lodge_hotel_harare.csv'
-output_file = 'cresta_lodge_hotel_harare_reviews.csv'
+input_file = 'Warthogs-Safari-Camp.csv'
+output_file = 'Warthogs-Safari-Camp-reviews.csv'
 
 start_date = datetime(2015, 1, 1)
 end_date = datetime(2023, 12, 31)
